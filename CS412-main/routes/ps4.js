4636029112262caf279b8bb69838f8d63a26847c
Koneshka <113035685+kon1402@users.noreply.github.com>
2024-05-07 15:00:43 -0400

    Add files via upload

--- CS412-main/routes/ps4.js
+++ CS412-main/routes/ps4.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var redis = require('redis');
+var axios = require('axios');
 var router = express.Router();
+
+var redisClient = redis.createClient();
 
 router.post('/', function(req, res, next) {
     const { key } = req.body;
@@ -9,9 +12,19 @@ router.post('/', function(req, res, next) {
         if (err) throw err;
 
         if (data !== null) {
-            res.json({ message: 'Retrieved from cache', data: JSON.parse(data) });
+            res.json({ message: 'Retrieved from cache', data: JSON.parse(data), fromCache: true });
         } else {
-            redisClient
+            axios.get('/forms')
+                .then(response => {
+                    const responseData = response.data;
+                    redisClient.setex(key, 15, JSON.stringify(responseData)); // Setting cache with 15-second timeout
+                    res.json({ message: 'Retrieved from third-party API', data: responseData, fromCache: false });
+                })
+                .catch(error => {
+                    res.status(500).json({ error: 'Internal server error' });
+                });
         }
-    })
-})
+    });
+});
+
+module.exports = router;
